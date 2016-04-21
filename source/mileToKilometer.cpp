b8d0f2c5feb37c673a5eaacf197ad9a8df4fc958
Jonas Dorsch <jesa8605@billl14.in.uni-weimar.de>
2016-04-21 14:20:01 +0200

    rework, mileToKilometer

--- source/mileToKilometer.cpp
+++ source/mileToKilometer.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <string>
 
-int calc(int mile){
-	int Kilometer=0;
+double calc(double mile){
+	double Kilometer=0;
 	Kilometer=mile/0.62137;
 	return Kilometer;
 	}
 
 int main()
 {
-	int mile;
+	double mile;
 	std::cout << "Bitte Meilen eingeben: " << std::endl;
 	std::cin >> mile;
 	std::cout << mile << " Meilen in Kilometer umgerechnet ergibt ca: " << calc(mile) << std::endl;
